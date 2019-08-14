93ecbf32006f8c55761a304fd3924b5bd1988769
billw2012 <billw2011@gmail.com>
2019-08-14 13:50:36 +0100

    FPK creation implemented, iterative patching not done yet.

--- FPKLive/Program.cs
+++ FPKLive/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,128 @@ namespace FPKLive
 {
     class Program
     {
-        static void Main(string[] args)
+        class FPKToken
         {
+            public string GitRevision;
+            public string[] ModifiedFiles;
+
+            public bool IsValid => GitRevision != null;
+        }
+
+        static void Log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static int Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: FPKLive <Mods\\Caveman2Cosmos directory>");
+                return 1;
+            }
+
+            var modDir = args[0];
+            var gitRootDir = Path.Combine(System.Reflection.Assembly.GetEntryAssembly().Location, "..");
+            var assetsDir = Path.Combine(gitRootDir, "Assets");
+            var toolsDir = Path.Combine(gitRootDir, "Tools");
+            var unpackedArtDir = Path.Combine(gitRootDir, "UnpackedArt");
+            var fpkTokenFile = Path.Combine(assetsDir, "fpklive_token.txt");
+
+            FPKToken token = ReadTokenFile(fpkTokenFile);
+            if(token == null)
+            {
+                // Calculate the FPK token
+                token = CreateToken(unpackedArtDir);
+                if(!token.IsValid)
+                {
+                    Log($"FPK token couldn't be calculated, are you sure you are in a git repository?");
+                }
+
+                // Create the FPKs themselves in temp dir
+                var tempFPKDir = Path.Combine(gitRootDir, "FPKTemp");
+                Directory.CreateDirectory(tempFPKDir);
+                ExecuteCommand("PakBuild", $"/I=\"{unpackedArtDir}\" /O=\"{tempFPKDir}\" /F /S=100 /R=C2C /X=bik", toolsDir);
+
+                // Delete old FPKs
+                foreach (var fpk in Directory.GetFiles(assetsDir, "*.fpk"))
+                {
+                    File.Delete(fpk);
+                }
+
+                // Move new FPKs to assets dir
+                foreach (var fpk in Directory.GetFiles(tempFPKDir, "*.fpk"))
+                {
+                    File.Move(fpk, Path.Combine(assetsDir, Path.GetFileName(fpk)));
+                }
+
+                // Cleanup
+                Directory.Delete(tempFPKDir);
+
+                // Save token (could do this as json or something but this is fine for now)
+                File.WriteAllLines(fpkTokenFile, new[] { token.GitRevision }.Concat(token.ModifiedFiles));
+            }
+
+            // Now we find the files that changed between the token and current working directory and copy those to the art directory, along with all the biks!
+
+
+            // 
+            // FPK is combination of git revision + modified files
+            // 
+            // When creating for the first time locally modified files need to be recorded as part of the FPK
+            // So FPK token is git revision + list of files modified when the FPK was made
+            // Files to keep lose are the list of files from the FPK token + all files listed in a dif between the git revision and HEAD
+            // 
+
+            // Diff different revisions "git --no-pager diff --name-only 777820db8d8490c113df5fc9db8809ff0a7610e6 66965b11a772c636e14a0340f0166010a9c6b6a0"
+            // List recent revisions "git log --pretty=format:%H -n 10"
+            // List locally modified files "git --no-pager diff --name-only HEAD"
+            // Get HEAD revision: "git rev-parse HEAD"
+            return 0;
+        }
+
+        static FPKToken ReadTokenFile(string fpkTokenFile)
+        {
+            if (File.Exists(fpkTokenFile))
+            {
+                var lines = File.ReadAllLines(fpkTokenFile);
+                if (lines.Length == 0)
+                {
+                    Log($"FPK Token file at {fpkTokenFile} is invalid, recreating FPKs");
+                }
+                else
+                {
+                    return new FPKToken
+                    {
+                        GitRevision = lines[0],
+                        ModifiedFiles = lines.Skip(1).ToArray()
+                    };
+                }
+            }
+            return null;
+        }
+
+        static FPKToken CreateToken(string rootDir)
+        {
+            return new FPKToken {
+                GitRevision = ExecuteGitCommand("rev-parse HEAD", rootDir).FirstOrDefault(),
+                ModifiedFiles = ExecuteGitCommand("diff --name-only HEAD", rootDir)
+            };
+        }
+
+        static string[] ExecuteGitCommand(string command, string workingDirectory) => ExecuteCommand("git", "--no-pager " + command, workingDirectory);
+
+        static string[] ExecuteCommand(string exe, string args, string workingDirectory)
+        {
+            var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = exe,
+                WorkingDirectory = workingDirectory,
+                Arguments = args,
+                RedirectStandardOutput = true,
+            });
+            process.WaitForExit();
+            return process.StandardOutput.ReadToEnd().Split('\n');
         }
     }
 }
